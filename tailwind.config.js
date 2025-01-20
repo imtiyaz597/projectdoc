d03a359fb539a9eae8d47660536eb15ae922b635
The_Akanksha <happychaudhary123spj@gmail.com>
2025-01-20 12:37:35 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '600px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      // example of a custom one
+      '2xl': '1440px',
+    },
+    extend: {
+      // other theme extensions
+    },
   },
   plugins: [],
 }
